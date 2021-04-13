31e82d69f89f5db22cdf22068e7aceec27ad26dc
guneet446 <guneet446@gmail.com>
2021-04-13 23:56:24 +0530

    Subject attendance increment decrement

--- lib/Attendance.dart
+++ lib/Attendance.dart
@@ -1,4 +1,6 @@
+import 'package:attendance/AttendanceCard.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'SubjectAttendanceDetails.dart';
 
 class Attendance extends StatefulWidget {
@@ -6,11 +8,71 @@ class Attendance extends StatefulWidget {
   _AttendanceState createState() => _AttendanceState();
 }
 
-List<SubjectAttendanceDetails> subject = [SubjectAttendanceDetails("Machine Learning", 12, 15), SubjectAttendanceDetails("Operating Systems", 5, 15), SubjectAttendanceDetails("Python", 17, 17)];
+List<SubjectAttendanceDetails> subjects = [SubjectAttendanceDetails("Machine Learning", 12, 15), SubjectAttendanceDetails("Operating Systems", 5, 15), SubjectAttendanceDetails("Python", 17, 17)];
 
 class _AttendanceState extends State<Attendance> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      appBar: PreferredSize(preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          title: Text('My Attendance', style: TextStyle( color: Colors.white, fontSize: 40,),),
+          backgroundColor: Colors.deepPurple,
+          centerTitle: true,
+        ),
+      ),
+      /*bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(26,10,26,10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Total = Rs ' + total.toString(),
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                ),),
+              RaisedButton(
+                onPressed: (){
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => Confirm())
+                  );
+                },
+                elevation: 8,
+                color: Color(0xffF2CC8F),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  'Continue',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Pacifico',
+                    color: Color(0xff3D405B),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        color: Color(0xffe29578),
+      ),*/
+      body: ListView.builder(
+        itemCount: subjects.length,
+        itemBuilder: (context, index) {
+          return AttendanceCard(
+            subject: subjects[index],
+            /*updateTotal: () {
+              setState(() {
+                foods.forEach((food) {total += food.amount;});
+              });
+            },*/
+          );
+        },
+      ),
+    );
   }
 }
--- lib/AttendanceCard.dart
+++ lib/AttendanceCard.dart
@@ -6,7 +6,7 @@ import 'package:attendance/SubjectAttendanceDetails.dart';
 class AttendanceCard extends StatefulWidget {
   final SubjectAttendanceDetails subject;
   //final Function updateTotal;
-  AttendanceCard({ this.subject});
+  AttendanceCard({this.subject});
 
   @override
   _AttendanceCardState createState() => _AttendanceCardState();
@@ -17,7 +17,7 @@ class _AttendanceCardState extends State<AttendanceCard> {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.fromLTRB(36, 16, 36, 0),
-      color: Color(0xffF4E5C3),
+      color: Colors.white70,
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
@@ -30,11 +30,11 @@ class _AttendanceCardState extends State<AttendanceCard> {
                   widget.subject.subjectName,
                   style: TextStyle(
                     fontSize: 22.0,
-                    color: Color(0xff3D405B),
+                    color: Colors.black,
                   ),
                 ),
                 ButtonBar(
-                  buttonMinWidth: 40,
+                  //buttonMinWidth: 20,
                   buttonHeight: 25,
                   children: <Widget>[
                     IconButton(
@@ -48,13 +48,6 @@ class _AttendanceCardState extends State<AttendanceCard> {
                         });
                       },
                     ),
-                    /*Text(
-                      widget.food.quantity.toString(),
-                      style: TextStyle(
-                        color: Color(0xff3D405B),
-                        fontSize: 18,
-                      ),
-                    ),*/
                     IconButton(
                       icon: Icon(Icons.cancel, color: Colors.red,),
                       onPressed: () {
@@ -69,7 +62,15 @@ class _AttendanceCardState extends State<AttendanceCard> {
                 ),
               ],
             ),
-            SizedBox(height: 6),
+            //SizedBox(height: 6),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  widget.subject.attended.toString() + ' / ' + widget.subject.total.toString(),
+                ),
+              ],
+            ),
             /*Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
